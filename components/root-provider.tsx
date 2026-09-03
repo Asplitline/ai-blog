4c56522dfaaa97a1f47f6e67668e9abcd1821b1b
Asplitline <893666612@qq.com>
2026-09-04 00:20:18 +0800

    feat: wire static search into RootProvider

---
+++ components/root-provider.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import type { ReactNode } from 'react';
+import { RootProvider } from 'fumadocs-ui/provider/next';
+import StaticSearchDialog from '@/components/search';
+
+export function AppRootProvider({ children }: { children: ReactNode }) {
+  return (
+    <RootProvider
+      search={{
+        SearchDialog: StaticSearchDialog,
+      }}
+    >
+      {children}
+    </RootProvider>
+  );
+}
